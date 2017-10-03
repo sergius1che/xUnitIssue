57da6023dd696ad86f4372be31f029ce6ec1ef60
live.in.gehenna <live.in.gehenna@gmail.com>
2017-10-03 11:31:40 +0300

    for iisue

---
+++ getDbVersion/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+
+namespace getDbVersion
+{
+    public class Class1
+    {
+        public string GetDbVersion(string connStr)
+        {
+            using (SqlConnection dbConnection = new SqlConnection(connStr))
+            {
+                dbConnection.Open();
+                return dbConnection.Query<string>("SELECT @@VERSION", commandTimeout: 600).Single();
+            }
+        }
+    }
+}
---
+++ xunitVsDapper/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace xunitVsDapper
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var gv = new getDbVersion.Class1();
+            string ver = gv.GetDbVersion("Data Source=HQ-IS-DB-D01\\SQLSERVER;Initial Catalog=ris;User ID=sa;Password=MSSQLadmin@1;Connect Timeout=600;");
+        }
+    }
+}
